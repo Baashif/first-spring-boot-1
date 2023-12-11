4907f52d76c6fb5961d2db67c49b041b9cb88527
h <@>
2023-12-11 23:48:07 +0300

    rr

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,79 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    public Long id;
+    public String name;
+    public String email;
+    public LocalDate dob;
+    public int age;
+
+    public Student(Long id, String name, String email, LocalDate dob, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dob, int age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
